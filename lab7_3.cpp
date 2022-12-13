5304b448251511a694f2eb70f8ab995a165b0ce1
Surapa <surapa152@gmail.com>
2022-12-13 22:59:40 +0700

    cp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 
+int adiff(int a, int b){
+  int c = a - b;
+  if ( c < 0 )
+  {
+    c = -c;
+  }
+  c = c % 360;
+  if ( c > 180)
+  {
+    c = 360 - c;
+  }
+  return c;
+}
 int main(){
   cout << adiff(180,270);
   cout << adiff(210,45);
